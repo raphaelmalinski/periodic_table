a2c21b136eab4bb8febfb8e6782dbd1596c372e6
Raphael <raphaelmalinski@gmail.com>
2021-01-15 21:44:29 +0000

    Creates basic periodic table layout

--- app/controllers/table_controller.rb
+++ app/controllers/table_controller.rb
@@ -2,5 +2,7 @@ class TableController < ApplicationController
   def index
     file = File.read("db/data.json")
     @elements = JSON.parse(file)
+    @x_size = (1..18)
+    @y_size = (1..8)
   end
 end
